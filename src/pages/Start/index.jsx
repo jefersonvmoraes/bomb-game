71922347bed0db128e3d0f37c376b117c245f0a8
Jeferson <jefersonvm2@hotmail.com>
2023-04-24 20:59:01 -0300

    Finalizando a pagina home, apenas o estilo

---
+++ src/pages/Start/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ButtonComponent from "../../components/Buttons";
+import { Container, Logo, Rules, SubTitle, Title } from "./styles";
+
+export default function Start(){
+
+    const handleNavToPlayAlone = () =>{
+        console.log("Teste de play alone")
+    }
+
+    const handleNavToPlayTogether = () =>{
+        console.log("Teste de play together")
+    }
+
+    const handleNavToRules = () => {
+        console.log("Teste de Rules")
+    }
+
+    return <Container>
+        <Logo
+            source={require("../../assets/logoDark.png")}
+            style={{resizeMode: 'contain'}}
+        />
+        <Title>Bem-vindo ao {"\n"} Bomb Game</Title>
+        <SubTitle>Escolha um modo de jogo.</SubTitle>
+        <ButtonComponent
+            buttonText={"Jogar Solo"}
+            handlePress={handleNavToPlayAlone}
+        />
+        <ButtonComponent
+            buttonText={"Jogar Em Dupla"}
+            handlePress={handleNavToPlayTogether}
+        />
+        <Rules onPress={handleNavToRules} >Ver as regras do jogo</Rules>
+    </Container>;
+}
